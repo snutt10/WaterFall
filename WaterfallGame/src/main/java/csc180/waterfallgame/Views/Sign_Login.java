6e63649beb7fbdb52cbe569592b51e1218d15924
snutt10 <snutt@student.neumont.edu>
2026-01-26 13:44:48 -0700

    Views classes

---
+++ WaterfallGame/src/main/java/csc180/waterfallgame/Views/Sign_Login.java
@@ -0,0 +1,18 @@
+package csc180.waterfallgame.Views;
+
+public class Sign_Login {
+    private String userName, password;
+    private int buttons;
+
+    public void login(String name, String password) {
+        //TODO: implement logic
+    }
+
+    public void signIn(String name, String password) {
+        //TODO: implement logic
+    }
+
+    public void loginOrSignIn() {
+
+    }
+}
