c3a816e86ee9e78705867136e885e588c02f2d01
snutt10 <snutt@student.neumont.edu>
2026-01-28 00:36:54 -0700

    trying to fix merge issue

--- WaterfallGame/src/main/java/csc180/waterfallgame/HelloApplication.java
+++ WaterfallGame/src/main/java/csc180/waterfallgame/HelloApplication.java
@@ -2,30 +2,31 @@ package csc180.waterfallgame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    PerspectiveCamera cam = new PerspectiveCamera();
+    Box box = new Box();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Race Cars");
+        scene.setCamera(cam);
 
-
-        stage.setScene(scene);
         stage.show();
     }
 
-
+    public void create3Dshape(){
+        box.setHeight(100);
+        box.setWidth(60);
+        box.setDepth(80);
+    }
 
     public static void main(String[] args) {
         launch();
