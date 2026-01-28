1aa308f94f38f57cde45819e63702144549f030b
LissAponte <japonterivera@student.neumont.edu>
2026-01-27 23:35:40 -0700

    Added logic for login, unsure about the sign in and passwords if still using.
    UI, logic for showRacers added. Still need the other methods and need JavaFx for the leaderBoard

--- WaterfallGame/src/main/java/csc180/waterfallgame/Views/UI.java
+++ WaterfallGame/src/main/java/csc180/waterfallgame/Views/UI.java
@@ -7,17 +7,23 @@ import java.util.List;
 public class UI {
     public void showRacers(List<Car> cars, int distance, float raceDuration) {
         //TODO: implement logic
+        for(Car car : cars ) {
+            System.out.println(car);
+        }
     }
 
     private void cars (List<Car> cars) {
         //Todo: implement logic
+
     }
 
     private void locationOfRacer(Car car){
         //TODO: implement logic
+
     }
 
     private void displayLeaderBoard(){
         //TODO: implement logic
+
     }
 }
